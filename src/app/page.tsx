1a0f096b104fc2c3d6aa0ac10d0bb7b6dd82bdfe
Mehmet Ali <malitopal113@gmail.com>
2025-08-29 21:01:57 +0200

    q

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import HeroSliderBS from "@/components/home/HeroSlider";
+import dynamic from "next/dynamic";
+
+const HeroSliderBS = dynamic(() => import("@/components/home/HeroSlider"), {
+  ssr: false, // ⬅️ kritik
+});
 
 export default function Home() {
   return (
