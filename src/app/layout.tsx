b8e687d861141fb16de3fb6935871bc7c6da7f8c
Mehmet Ali <malitopal113@gmail.com>
2025-08-29 20:45:19 +0200

    ssr editted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Cabin } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header/header";
-import Footer from "@/components/footer/footer";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import dynamic from "next/dynamic";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const Header = dynamic(() => import("@/components/header/header"), { ssr: false });
+const Footer = dynamic(() => import("@/components/footer/footer"), { ssr: false });
+
+
+
 
 const cabin = Cabin({
   subsets: ["latin"],
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import Image from "next/image";
 
 
