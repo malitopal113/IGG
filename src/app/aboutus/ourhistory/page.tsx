41260dc5d27ffe62c20a913f48e3ef8492657324
Mehmet Ali <malitopal113@gmail.com>
2025-09-12 05:36:33 +0200

    efeziofezf

--- src/app/aboutus/ourhistory/page.tsx
+++ src/app/aboutus/ourhistory/page.tsx
@@ -1,11 +1,15 @@
 import Hero1 from "./Hero1";
-
+import Hero2 from "./Hero2";
+import Hero3 from "./Hero3";
 
 
 export default function AboutUsPage() {
   return (
     <main className="w-full min-h-screen">
       <Hero1 />
+      <Hero2 />
+      <Hero3 />
+
       
     </main>
   );
